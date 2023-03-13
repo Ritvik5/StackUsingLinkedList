7faeede79d756560037646372ee729eef88fd74c
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-13 17:30:59 +0530

    Implementing Stack Using LinkedList

---
+++ LinkedListStack/LinkListStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListStack
+{
+    public class LinkListStack
+    {
+        private Node top;
+        
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;//1400
+            }
+        }
+    }
+}
---
+++ LinkedListStack/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedListStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkListStack linkListStack = new LinkListStack();
+            linkListStack.Push(70);
+            linkListStack.Push(30);
+            linkListStack.Push(56);
+            linkListStack.Display();
+        }
+    }
+}
